2e1b4ec52dee5daeced5032ea02fd247f6d0c14e
Pratik Gaikwad <pratikgaikwad8586@gmail.com>
2024-01-30 09:28:44 +0530

    fix

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4THK4GCA.mjs";import"./chunk-446RXYJ5.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-PVBRRYEE.mjs";import"./chunk-446RXYJ5.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
